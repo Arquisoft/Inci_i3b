e901633ffccc2c444cb0bfb6a932990dfc6a8a7f
admipa97 <uo251235@uniovi.es>
2018-04-25 20:24:58 +0200

    New APIIncidenceController defined in order to create incidences without the agents web interface

--- Agents_i3b/src/main/java/uniovi/es/services/InsertSampleDataService.java
+++ Agents_i3b/src/main/java/uniovi/es/services/InsertSampleDataService.java
@@ -18,7 +18,7 @@ public class InsertSampleDataService {
 		Agent agent1 = new Agent("11111111A","123456","Pepe","pepe@gmail.com","Oviedo","Person","1");
 		agentsRepository.save(agent1);
 		Agent sensor = new Agent("sensor","123","Temperature Sensor","pepe@gmail.com","Oviedo","Sensor","3");
-		agentsRepository.save(agent1);
+		agentsRepository.save(sensor);
 //		Agent agent2 = new Agent("22222222B","123456","Jose","jose@gmail.com","Gijón","Person","1");
 //		agentsRepository.save(agent2);
 //		Agent agent3 = new Agent("33333333C","123456","Pelayo","pelayo@gmail.com","Pravia","Person","1");
